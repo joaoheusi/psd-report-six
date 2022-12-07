b77d90e0f59ba3af9fb0e27781b2cf898bc961ff
João Marcelo Heusi <jmarcelo.heusi@gmail.com>
2022-12-07 11:18:30 -0300

    add adder and mux_3x1

--- src/adder.vhd
+++ src/adder.vhd
@@ -1,2 +1,17 @@
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
+USE ieee.std_logic_unsigned.ALL;
+
+ENTITY adder IS
+  PORT (
+    i_A : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
+    i_B : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
+    o_C : OUT STD_LOGIC_VECTOR(15 DOWNTO 0)
+  );
+END adder;
+ARCHITECTURE arch_1 OF adder IS
+BEGIN
+
+  o_C <= i_A + i_B;
+
+END ARCHITECTURE arch_1
--- src/mux_3x1.vhd
+++ src/mux_3x1.vhd
@@ -1,2 +1,26 @@
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
+ENTITY mux3_1x16bit IS
+  PORT (
+    i_SEL : IN STD_LOGIC_VECTOR(1 DOWNTO 0); -- selector
+    i_A : IN STD_LOGIC_VECTOR(15 DOWNTO 0); -- data input
+    i_B : IN STD_LOGIC_VECTOR(15 DOWNTO 0); -- data input
+    i_C : IN STD_LOGIC_VECTOR(15 DOWNTO 0); -- data input
+    o_S : OUT STD_LOGIC_VECTOR(15 DOWNTO 0)); -- data output
+END mux3_1x16bit;
+
+ARCHITECTURE arch_1 OF mux3_1x16bit IS
+BEGIN
+  PROCESS (i_SEL, i_A, i_B, i_C)
+  BEGIN
+    IF (i_SEL = "00") THEN
+      o_S <= i_A;
+    ELSIF (i_SEL = "01") THEN
+      o_S <= i_B;
+    ELSIF (i_SEL = "10") THEN
+      o_S <= i_C;
+    ELSE
+      o_S <= "0000000000000000";
+    END IF;
+  END PROCESS;
+END arch_1;
